df6c76d4e17a1ac97cf35bd3f8a5d19c8e5184f5
LoelDev <pmpinzasr@hotmail.com>
2021-11-06 15:28:57 -0500

    feature: Add Profile Endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './Security/user/Modules/user.module';
 import { User } from './Security/user/Entities/user.entity';
 import { CPost } from './Publishing & Social Interaction/commonPost/Entities/post.entity';
+import { PostModule } from './Publishing & Social Interaction/commonPost/Modules/post.module';
+import { ProfileModule } from './Profile/profile/Modules/profile.module';
+import { Profile } from './Profile/profile/Entities/profile.entity';
 
 @Module({
   imports: [
@@ -15,11 +18,13 @@ import { CPost } from './Publishing & Social Interaction/commonPost/Entities/pos
       username: 'root',
       password: '123456',
       database: 'producersprueba',
-      entities: [User, CPost],
+      entities: [User, CPost, Profile],
       synchronize: true,
       dropSchema: false,
     }),
     UserModule,
+    PostModule,
+    ProfileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
